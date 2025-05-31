5023d02d92f6746f8b3627a4fae0675750238505
ConiStuff <conishadsy@gmail.com>
2025-05-30 22:04:08 -0400

    saddfs

---
+++ AncientCTRLALTDEL/Form1.Designer.vb
@@ -0,0 +1,103 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox
+        Me.warn = New System.Windows.Forms.Label
+        Me.leave = New System.Windows.Forms.Button
+        Me.endtsk = New System.Windows.Forms.Button
+        Me.shutdown = New System.Windows.Forms.Button
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(12, 12)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(300, 147)
+        Me.ListBox1.TabIndex = 0
+        '
+        'warn
+        '
+        Me.warn.AutoSize = True
+        Me.warn.Location = New System.Drawing.Point(9, 166)
+        Me.warn.Name = "warn"
+        Me.warn.Size = New System.Drawing.Size(294, 39)
+        Me.warn.TabIndex = 1
+        Me.warn.Text = "WARNING: Pressing CTRL+ALT+DEL again will restart your " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "computer. You will lose " & _
+            "unsaved information in all programs " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "that are running,"
+        '
+        'leave
+        '
+        Me.leave.Location = New System.Drawing.Point(212, 226)
+        Me.leave.MaximumSize = New System.Drawing.Size(100, 23)
+        Me.leave.MinimumSize = New System.Drawing.Size(100, 23)
+        Me.leave.Name = "leave"
+        Me.leave.Size = New System.Drawing.Size(100, 23)
+        Me.leave.TabIndex = 4
+        Me.leave.Text = "&Cancel"
+        Me.leave.UseVisualStyleBackColor = True
+        '
+        'endtsk
+        '
+        Me.endtsk.Location = New System.Drawing.Point(12, 226)
+        Me.endtsk.Name = "endtsk"
+        Me.endtsk.Size = New System.Drawing.Size(99, 23)
+        Me.endtsk.TabIndex = 5
+        Me.endtsk.Text = "&End Task"
+        Me.endtsk.UseVisualStyleBackColor = True
+        '
+        'shutdown
+        '
+        Me.shutdown.Location = New System.Drawing.Point(117, 226)
+        Me.shutdown.Name = "shutdown"
+        Me.shutdown.Size = New System.Drawing.Size(89, 23)
+        Me.shutdown.TabIndex = 6
+        Me.shutdown.Text = "&Shut Down"
+        Me.shutdown.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(324, 261)
+        Me.Controls.Add(Me.shutdown)
+        Me.Controls.Add(Me.endtsk)
+        Me.Controls.Add(Me.leave)
+        Me.Controls.Add(Me.warn)
+        Me.Controls.Add(Me.ListBox1)
+        Me.MaximumSize = New System.Drawing.Size(332, 288)
+        Me.MinimumSize = New System.Drawing.Size(332, 288)
+        Me.Name = "Form1"
+        Me.Text = "Close Program"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents warn As System.Windows.Forms.Label
+    Friend WithEvents leave As System.Windows.Forms.Button
+    Friend WithEvents endtsk As System.Windows.Forms.Button
+    Friend WithEvents shutdown As System.Windows.Forms.Button
+
+End Class
---
+++ AncientCTRLALTDEL/Form1.vb
@@ -0,0 +1,83 @@
+﻿Imports System.Diagnostics
+
+Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.ControlBox = False
+        RefreshTaskList()
+    End Sub
+
+    Private Sub RefreshTaskList()
+        ListBox1.Items.Clear()
+
+        Dim explorerTitle As String = ""
+        Dim tasks As New List(Of String)
+
+        Dim currentProcessId As Integer = Process.GetCurrentProcess().Id
+
+        For Each p As Process In Process.GetProcesses()
+            Try
+                If p.MainWindowHandle <> IntPtr.Zero AndAlso Not String.IsNullOrEmpty(p.MainWindowTitle) Then
+                    If p.Id = currentProcessId Then Continue For
+
+                    If p.ProcessName.ToLower() = "explorer" Then
+                        explorerTitle = p.MainWindowTitle
+                    Else
+                        tasks.Add(p.MainWindowTitle)
+                    End If
+                End If
+            Catch ex As Exception
+
+            End Try
+        Next
+        If explorerTitle <> "" Then
+            ListBox1.Items.Add(explorerTitle)
+        End If
+
+        For Each title As String In tasks
+            ListBox1.Items.Add(title)
+        Next
+    End Sub
+
+    Private Sub leave_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles leave.Click
+        Me.Close()
+    End Sub
+
+    Private Sub endtsk_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles endtsk.Click
+        If ListBox1.SelectedItem Is Nothing Then
+            MessageBox.Show("Please select a task to end.")
+            Return
+        End If
+
+        Dim selectedTitle As String = ListBox1.SelectedItem.ToString()
+
+        For Each p As Process In Process.GetProcesses
+            Try
+                If p.MainWindowHandle <> IntPtr.Zero AndAlso p.MainWindowTitle = selectedTitle Then
+                    p.Kill()
+
+                    RefreshTaskList()
+                    Return
+                End If
+            Catch ex As Exception
+                MessageBox.Show("unable to end task.")
+            End Try
+        Next
+        MessageBox.Show("task not found.")
+
+    End Sub
+
+    Private Sub shutdown_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles shutdown.Click
+        Try
+            Dim psi As New ProcessStartInfo()
+            psi.FileName = "shutdown"
+            psi.Arguments = "/s /t 0"
+            psi.CreateNoWindow = True
+            psi.UseShellExecute = False
+
+            Process.Start(psi)
+        Catch ex As Exception
+            MessageBox.Show("An unexpected error is keeping Windows from shutting down.")
+        End Try
+    End Sub
+End Class
